f92a24732e07f78921cf8241ad43d4694430d059
bob.w.perez <bob.w.perez@gmail.com>
2021-08-17 18:34:22 -0400

    fixed error in service-worker, number 3

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,69 +27,66 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        // return array of cache names that are old to delete
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", function (evt) {
+self.addEventListener("fetch", (event) => {
+  // non GET requests are not cached and requests to other origins are not cached
   if (
-    evt.request.method !== "GET" ||
-    !evt.request.url.startsWith(self.location.origin)
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
   ) {
-    evt.respondWith(fetch(evt.request));
+    event.respondWith(fetch(event.request));
     return;
   }
 
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/images")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
     );
-
     return;
   }
 
-  evt.respondWith(
-    caches.match(evt.request).then((cachedResponse) => {
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
         return cachedResponse;
       }
 
       // request is not in cache. make network request and cache the response
       return caches.open(RUNTIME_CACHE).then((cache) => {
-        return fetch(evt.request).then((response) => {
-          return cache.put(evt.request, response.clone()).then(() => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
             return response;
           });
         });
