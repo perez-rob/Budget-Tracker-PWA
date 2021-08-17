44b0a86c1705a7c051523dad6912832f29a9bf78
bob.w.perez <bob.w.perez@gmail.com>
2021-08-17 16:03:18 -0400

    ran webpack and recieved dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
-  entry: "./public/assets/js/app.js",
+  entry: "./public/index.js",
   output: {
     path: __dirname + "/public/dist",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   mode: "production",
   plugins: [
@@ -21,8 +21,8 @@ const config = {
       // files predictable making it easier to refer to them in our code
       fingerprints: false,
 
-      name: "Images App",
-      short_name: "Images App",
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
       theme_color: "#ffffff",
       background_color: "#ffffff",
       start_url: "/",
@@ -30,17 +30,14 @@ const config = {
 
       icons: [
         {
-          src: path.resolve(
-            __dirname,
-            "public/assets/images/icons/icon-512x512.png"
-            ),
+          src: path.resolve(__dirname, "public/icons/icon-512x512.png"),
           // the plugin will generate an image for each size
           // included in the size array
-          size: [72, 96, 128, 144, 152, 192, 384, 512]
-        }
-      ]
-    })
-  ]
+          size: [72, 96, 128, 144, 152, 192, 384, 512],
+        },
+      ],
+    }),
+  ],
 };
 
 module.exports = config;
